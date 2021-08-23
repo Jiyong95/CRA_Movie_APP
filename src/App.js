9961bf9b5c1b6387b8854a1781dd4b4f22332e5b
jiyong LEE <jyman1995@gmail.com>
2021-08-23 18:51:29 +0900

    Make Movie Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function App() {
-  return <div className="App"/>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((state) => ({ isLoading: false }));
+    }, 6000);
+  }
+  render() {
+    //구조 분해 할당
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
 
 export default App;
